97111b2ce813186306f542c859717fe7fc602163
Jeff Romine <jromineut@gmail.com>
2013-01-17 12:37:34 -0800

    rearranged for readability

--- lib/jpdfer/split.rb
+++ lib/jpdfer/split.rb
@@ -13,7 +13,7 @@ module Jpdfer
     def build_interval_list_list(interval_strings)
       interval_strings.split(',').map {|s| [string_to_interval(s)]}
     end
-
+    
     def reduce_interval_list_list(interval_list_list)
       interval_list_list.reduce([[]]) do |result,element|
         new_last,remainder=merge_interval_list_lists(result.last,element)
@@ -25,10 +25,19 @@ module Jpdfer
       end
     end
 
-    def replace_last(list,new_last)
-      new_list=list.clone
-      new_list[-1]=new_last
-      new_list
+    def build_interval_string_list(interval_list_list)
+      interval_list_list.map {|intervals| build_interval_string(intervals)}
+    end
+    
+    def string_to_interval(s)
+      if s.nil? || s.empty?
+        return nil
+      end
+      
+      interval=s.split('-').map{|s| verified_to_i(s)}
+      return interval if interval.size == 2
+      return interval << interval[0] if interval.size == 1
+      return nil
     end
     
     def merge_interval_list_lists(list_a,list_b)
@@ -50,9 +59,25 @@ module Jpdfer
       end
     end
 
+    def build_interval_string(intervals)
+      intervals.map do |i|
+        i0,i1=i
+        i0 == i1 ? "#{i0}" : "#{i0}-#{i1}"
+      end.join(',')
+    end
+
+    def verified_to_i(s)
+      result = s.to_i
+      if result.to_s == s
+        result
+      else
+        raise StandardError.new('invalid integer: ' + s)
+      end
+    end
+    
     def can_merge?(interval_list,interval)
       !interval_list.nil? &&
-      !interval_list.any? {|list_interval| overlapping?(list_interval,interval)}
+        !interval_list.any? {|list_interval| overlapping?(list_interval,interval)}
     end
     
     def merge_interval(interval_list,interval)
@@ -66,18 +91,16 @@ module Jpdfer
 
       interval_list.clone << interval
     end
-    
-    def build_interval_string_list(interval_list_list)
-      interval_list_list.map {|intervals| build_interval_string(intervals)}
+
+    def overlapping?(a,b)
+      a0,a1=a
+      b0,b1=b
+      within?(a0,b0,b1) ||
+        within?(a1,b0,b1) ||
+        within?(b0,a0,a1) ||
+        within?(b1,a0,a1)
     end
-    
-    def build_interval_string(intervals)
-      intervals.map do |i|
-        i0,i1=i
-        i0 == i1 ? "#{i0}" : "#{i0}-#{i1}"
-      end.join(',')
-    end
-    
+
     def adjoining?(a,b)
       a0,a1=a
       b0,b1=b
@@ -91,37 +114,14 @@ module Jpdfer
       [a[0],b[1]]
     end
     
-    def overlapping?(a,b)
-      a0,a1=a
-      b0,b1=b
-      within?(a0,b0,b1) ||
-      within?(a1,b0,b1) ||
-      within?(b0,a0,a1) ||
-      within?(b1,a0,a1)
+    def replace_last(list,new_last)
+      new_list=list.clone
+      new_list[-1]=new_last
+      new_list
     end
 
     def within?(a,b,c)
       a >= b && a <= c || a >= c && a <= b
-    end
-    
-    def verified_to_i(s)
-      result = s.to_i
-      if result.to_s == s
-        result
-      else
-        raise StandardError.new('invalid integer: ' + s)
-      end
-    end
-    
-    def string_to_interval(s)
-      if s.nil? || s.empty?
-        return nil
-      end
-      
-      interval=s.split('-').map{|s| verified_to_i(s)}
-      return interval if interval.size == 2
-      return interval << interval[0] if interval.size == 1
-      return nil
     end
   end
 end
